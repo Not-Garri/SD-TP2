7fc11c0a826977330ba543a9f86403ee1b100623
Not-Garri <rodrigogarraio@gmail.com>
2024-11-24 16:05:32 +0000

    TODO removido do ficheiro

--- src/Comunication/ServerInterface.java
+++ src/Comunication/ServerInterface.java
@@ -13,16 +13,6 @@ import java.util.ArrayList;
 
 //Metodos que vao ser executados no servidor
 public interface ServerInterface extends Remote {
-
-    //TODO: Validar Login (basicamente metodo login que recebe o username e a password)
-    /*
-    TODO: Funcionalidades de produtos:
-     - Listar todos os produtos
-     - Atualizar preco de um dado produto (o que deve alterar o ficheiro)
-     - adicionar produtos (o que deve alterar o ficheiro)
-     - Notificar todos os clientes de que um produto foi alterado (maybe client-side, idk)
-     */
-
     ComunicationCode login(String username, String password, ClientInterface clientInterface) throws RemoteException;
     ArrayList<String> searchProductByName(String productName) throws RemoteException;
     ArrayList<String> searchProductbyStore(String storeName) throws RemoteException;
